31b6c06172451f23c19d4376cbff61096318bbd7
Kaushal19 <koushal141@gmail.com>
2022-02-28 12:00:20 +0530

    Clean up

--- pages/api/recommend.js
+++
@@ -1,8 +0,0 @@
-import data from "../../data/DisneyData.json";
-
-export default function (req, res) {
-  const { movies } = data;
-  const Movies = Object.values(movies);
-  const toSend = Movies.filter((fil) => fil.type == "recommend");
-  res.json(toSend);
-}
